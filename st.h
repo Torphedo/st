512c8dbd69154eabfae3e0227eccb59962c2cf2c
torph <73564623+Torphedo@users.noreply.github.com>
2023-11-24 01:39:44 -0500

    Apply custom font, scrollback & new instance patches Adds support for mouse scrolling via a ring buffer, and will open a new st terminal after pressing Ctrl+Shift+Return.

--- st.h
+++ st.h
@@ -19,6 +19,7 @@
 
 #define TRUECOLOR(r,g,b)	(1 << 24 | (r) << 16 | (g) << 8 | (b))
 #define IS_TRUECOL(x)		(1 << 24 & (x))
+#define HISTSIZE            2000
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
@@ -81,12 +82,14 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void newterm(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
 void toggleprinter(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);
